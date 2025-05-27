acd7fecc8ea9e2cfdcd0f425353fb1b651f04f05
Vadim Rutkovsky <vrutkovs@redhat.com>
2025-05-27 08:59:48 +0200

    Convert into project webhook

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM ubuntu:20.10
+FROM registry.access.redhat.com/ubi9/ubi:9.4
 
-RUN apt-get update -y && apt-get install -y python3-pip python-dev
+RUN dnf install -y python3-pip python3-devel
 
 WORKDIR /app
 
@@ -12,4 +12,4 @@ COPY validate.py /app
 
 COPY wsgi.py /app
 
-CMD gunicorn --certfile=/certs/webhook.crt --keyfile=/certs/webhook.key --bind 0.0.0.0:443 wsgi:webhook
+CMD gunicorn --certfile=/certs/tls.crt --keyfile=/certs/tls.key --bind 0.0.0.0:9443 wsgi:webhook
--- validate.py
+++ validate.py
@@ -8,23 +8,13 @@ webhook.config['LABEL'] = environ.get('LABEL')
 
 webhook.logger.setLevel(logging.INFO)
 
-
-if "LABEL" not in environ:
-    webhook.logger.error("Required environment variable for label isn't set. Exiting...")
-    exit(1)
-
-
 @webhook.route('/validate', methods=['POST'])
 def validating_webhook():
     request_info = request.get_json()
     uid = request_info["request"].get("uid")
+    webhook.logger.info(f"request:\n{request_info}")
 
-    if request_info["request"]["object"]["metadata"]["labels"].get(webhook.config['LABEL']):
-        webhook.logger.info(f'Object {request_info["request"]["object"]["kind"]}/{request_info["request"]["object"]["metadata"]["name"]} contains the required \"{webhook.config["LABEL"]}\" label. Allowing the request.')
-        return admission_response(True, uid, f"{webhook.config['LABEL']} label exists.")
-    else:
-        webhook.logger.error(f'Object {request_info["request"]["object"]["kind"]}/{request_info["request"]["object"]["metadata"]["name"]} doesn\'t have the required \"{webhook.config["LABEL"]}\" label. Request rejected!')
-        return admission_response(False, uid, f"The label \"{webhook.config['LABEL']}\" isn't set!")
+    return admission_response(True, uid, "lets go")
 
 
 def admission_response(allowed, uid, message):
